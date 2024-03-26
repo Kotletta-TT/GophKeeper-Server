c81ee85da788b6da45e89b9c7181a4039ae2849d
Рейсман Вячеслав <vareysman@sberbank.ru>
2024-03-26 19:18:42 +0300

    init files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GophKeeper-Server
 
 go 1.22.0
+
+require golang.org/x/sync v0.6.0
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,19 @@
 package app
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func Run(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			fmt.Println("hello")
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
